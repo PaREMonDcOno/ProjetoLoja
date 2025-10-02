3d9954d87a494e5228f87a2f6e1c5980acdb3af2
Lucas <ltelesduarte@gmail.com>
2025-10-02 16:06:12 -0300

    Baixando o ProjetoLoja

---
+++ ProjetoLoja/Controllers/UsuarioController.cs
@@ -0,0 +1,47 @@
+﻿//IMPORTANDO PACOTES PARA UTILIZAR NO PROJETO
+using Microsoft.AspNetCore.Mvc;
+using ProjetoLoja.Repositorio;
+
+//DEFINE O NOME E AONDE A CLASSE ESTA LOCALIZADA, NAMESPACE AJUDA A ORGANIZAR O CODIGO
+//E EVITAR CONFLITOS DE NOMES
+namespace ProjetoLoja.Controllers
+{
+    //CLASSE USUARIOCONTROLLER QUE ESTA HERDANDO DA CLASSE CONTROLLER
+    public class UsuarioController : Controller
+    {
+        //DECLARA UMA VARIAVEL PRIVADA SOMENTE LEITURA DO TIPO USUARIOREPOSITORIO CHAMADA(INSTANCIANDO) _usuarioRepositorio;
+        //UsuarioRepositorio É UMA CLASSE RESPONSAVEL POR INTERAGIR COM A CAMADA DE DADOS E GERENCIA AS INFORMAÇÕES DO USUARIO;
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        // CONSTRUTOR QUE RECEBE A INSTANCIA USUARIOREPOSITORIO COM PARAMETROS (INJEÇÃO DE DEPENDECIA)
+        //PARA SER INICIALIZADO.
+
+        public UsuarioController(UsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        //INTERFACE É UMA REPRESENTAÇAO DO RESULTADO (TELA)
+        public IActionResult Login()
+        {
+            //RETORNA A PAGINA INDEX
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            var usuario = _usuarioRepositorio.ObterUsuario(email);
+
+            if(usuario != null && usuario.senha != senha)
+            {
+                return RedirectToAction("Index", "Cliente");
+            }
+            ModelState.AddModelError("", "Email / senha Inválidos");
+
+
+            //RETORNA A PAGINA INDEX
+            return View();
+        }
+    }
+}
---
+++ script.sql
@@ -0,0 +1,25 @@
+-- CRIANDO O BANCO DE DADOS
+CREATE DATABASE dbloja;
+
+-- USANDO O BANCO DE DADOS
+USE dbloja;
+
+-- CRIANDO AS TABELAS DO BANCO DE DADDOS
+CREATE TABLE Usuario(
+id int primary key auto_increment,
+nome varchar(40) not null,
+email varchar(40) not null,
+senha varchar(40) not null
+);
+
+CREATE TABLE Cliente (
+codCli int primary key auto_increment,
+nomeCli varchar(40) not null,
+telCli varchar(40) not null,
+emailCli varchar(40) not null
+);
+
+-- CONSULTANDO AS TABELAS DO BANC0
+
+SELECT * FROM Usuario;
+SELECT * FROM Cliente;
